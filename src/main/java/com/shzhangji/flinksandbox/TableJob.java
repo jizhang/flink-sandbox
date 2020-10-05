128b6445ecfc382a493637fbd3bb67cab487ae70
Ji ZHANG <zhangji87@gmail.com>
2020-10-05 16:16:34 +0800

    wordcount with blink sql

--- src/main/java/com/shzhangji/flinksandbox/TableJob.java
+++ src/main/java/com/shzhangji/flinksandbox/TableJob.java
@@ -1,41 +1,45 @@
 package com.shzhangji.flinksandbox;
 
-import org.apache.flink.api.common.typeinfo.Types;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.annotation.FunctionHint;
 import org.apache.flink.table.api.EnvironmentSettings;
-import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.functions.TableFunction;
-import org.apache.flink.table.sources.CsvTableSource;
-import org.apache.flink.table.sources.TableSource;
 import org.apache.flink.types.Row;
 
 public class TableJob {
   public static void main(String[] args) throws Exception {
-    StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
     EnvironmentSettings settings = EnvironmentSettings.newInstance()
       .useBlinkPlanner()
-      .inStreamingMode()
+      .inBatchMode()
       .build();
-    StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
+    TableEnvironment tableEnv = TableEnvironment.create(settings);
 
-    TableSource source = new CsvTableSource.Builder()
-      .path("data/wordcount.txt")
-      .field("line", Types.STRING)
-      .build();
-    tableEnv.registerTableSource("wordcount", source);
-    tableEnv.registerFunction("TOKENIZER", new Tokenizer());
-    Table wordcountTable = tableEnv.sqlQuery(
-      "SELECT word, COUNT(*) AS cnt" +
-        " FROM wordcount, LATERAL TABLE(TOKENIZER(line)) AS t (word)" +
-        " GROUP BY word");
-    DataStream<?> stream = tableEnv.toRetractStream(wordcountTable, Row.class);
-    stream.print();
+    String path = System.getProperty("user.dir") + "/data/wordcount.txt";
+    tableEnv.executeSql("CREATE TABLE wordcount (\n" +
+      "  line STRING\n" +
+      ")\n" +
+      "WITH (\n" +
+      "  'connector' = 'filesystem'\n" +
+      "  ,'path' = '" + path + "'\n" +
+      "  ,'format' = 'csv'\n" +
+      ")");
 
-    env.execute();
+    tableEnv.executeSql("CREATE TABLE wordcount_result (\n" +
+      "  word STRING\n" +
+      "  ,cnt BIGINT\n" +
+      ") WITH ('connector' = 'print')");
+
+    tableEnv.createFunction("TOKENIZER", Tokenizer.class);
+
+    tableEnv.executeSql(
+      "INSERT INTO wordcount_result\n" +
+      "SELECT word, COUNT(*)\n" +
+      "FROM wordcount, LATERAL TABLE(TOKENIZER(line)) AS t(word)\n" +
+      "GROUP BY word");
   }
 
+  @FunctionHint(output = @DataTypeHint("ROW<word STRING>"))
   public static class Tokenizer extends TableFunction<Row> {
     public void eval(String line) {
       for (String word : line.split("\\s+")) {
